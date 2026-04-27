964371f7410590f2071f6fa69ca01fdd5ad0bb5e
wnsgur71-crypto <wnsgur71@gmail.com>
2026-04-27 20:29:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,11 +15,20 @@ ws.on("message", (data) => {
   const message = JSON.parse(data.toString());
   console.log("Received:", message);
 
-  // Sinric 명령 응답
-  if (message.action === "setPowerState") {
+  // heartbeat 무시
+  if (!message.payload) return;
+
+  if (message.payload.action === "setPowerState") {
+    console.log("Switch:", message.payload.value.state);
+
     const response = {
-      requestId: message.requestId,
+      header: {
+        payloadVersion: 2
+      },
       payload: {
+        deviceId: message.payload.deviceId,
+        replyToken: message.payload.replyToken,
+        type: "response",
         success: true
       }
     };
