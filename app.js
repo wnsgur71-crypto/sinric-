32f0e6fbf50cc7e94707479ed7424c6fb4ccb776
wnsgur71-crypto <wnsgur71@gmail.com>
2026-04-27 19:49:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ const WebSocket = require("ws");
 
 const ws = new WebSocket("wss://ws.sinric.pro", {
   headers: {
-    appkey: "YOUR_APP_KEY",
-    authorization: "YOUR_APP_SECRET"
+    appkey: process.env.APP_KEY,
+    authorization: process.env.APP_SECRET
   }
 });
 
 ws.on("open", () => {
-  console.log("Connected!");
+  console.log("Sinric connected");
 });
 
 ws.on("message", (data) => {
-  console.log(data.toString());
+  const msg = JSON.parse(data);
+  console.log("Received:", msg);
+
+  // 명령 받으면 성공 응답
+  if (msg.deviceId === process.env.DEVICE_ID) {
+    ws.send(JSON.stringify({
+      deviceId: process.env.DEVICE_ID,
+      success: true
+    }));
+  }
 });
 
 ws.on("close", () => {
